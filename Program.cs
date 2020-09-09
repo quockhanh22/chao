a14c75dce11cf430437acaaadcab9a29f85aceec
quockhanh22 <anhkhanh11761@gmail.com>
2020-09-09 21:17:54 +0700

    welcome
    welcome

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace welcome
+{
+    class Program
+    {
+    
+
+        static void Main(string[] args)
+        {
+          chao();
+        }
+        static void chao()
+        {
+            Console.WriteLine("What is your name ?");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello {0} ! ", name);
+        }
+    }
+}
